7c154e3252be45bb86c0b0242a6ab948c742a6d8
Carrick Rogers <carrickr@kongo.lan>
2022-05-29 19:26:13 -0700

    make rubocop happy

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 class Attachment < ApplicationRecord
   validates :stored_in, presence: true
 
   enum stored_in: { local: 0 }
-
-
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
- it { should define_enum_for(:stored_in) }
- it { should validate_presence_of(:stored_in) }
+  it { should define_enum_for(:stored_in) }
+  it { should validate_presence_of(:stored_in) }
 end
