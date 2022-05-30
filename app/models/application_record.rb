02553562badc323f2e9fa3630f4157df17fa16f1
Carrick Rogers <carrickr@kongo.lan>
2022-05-29 19:06:13 -0700

    attachment model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  include ULID::Rails
+  ulid :id, auto_generate: true
 end
---
+++ db/migrate/20220530014724_create_attachments.rb
@@ -0,0 +1,15 @@
+class CreateAttachments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :attachments, id: false do |t|
+      t.binary :id, limit: 16, primary_key: true
+      t.string :file_name
+      t.string :mime_type
+      t.boolean :active, default: true
+      t.datetime :upload_at
+      t.datetime :processed_at
+      t.string :md5_checksum
+      
+      t.timestamps
+    end
+  end
+end
